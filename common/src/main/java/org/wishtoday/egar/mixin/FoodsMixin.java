fb9b01edd17da1106d54eaa5defc6331846ec23c
wishtoday238 <wishtoday238@gmail.com>
2025-10-11 16:36:08 +0800

    fix 1.21.1forge

--- common/src/main/java/org/wishtoday/egar/mixin/FoodsMixin.java
+++ common/src/main/java/org/wishtoday/egar/mixin/FoodsMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings("DuplicatedCode")
 @Mixin(Foods.class)
 public class FoodsMixin {
     @Mutable
