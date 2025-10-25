cc166417c5eccc467d5baff68e9e06e055b95e15
wishtoday238 <wishtoday238@gmail.com>
2025-10-25 20:14:43 +0800

    merge betterRecipe to 1.21.4, stop support 1.21~ forge version.

---
+++ common/src/main/java/org/wishtoday/egar/mixin/goldApple/EnchantmentScreenHandlerMixin.java
@@ -0,0 +1,27 @@
+package org.wishtoday.egar.mixin.goldApple;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EnchantmentMenu.class)
+public class EnchantmentScreenHandlerMixin {
+    @Shadow
+    @Final
+    private Container enchantSlots;
+
+    @Inject(method = "clickMenuButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ContainerLevelAccess;execute(Ljava/util/function/BiConsumer;)V", shift = At.Shift.AFTER))
+    private void onButtonClick(Player pPlayer, int pId, CallbackInfoReturnable<Boolean> cir) {
+        ItemStack stack = this.enchantSlots.getItem(0);
+        if (!stack.is(Items.GOLDEN_APPLE)) return;
+        this.enchantSlots.setItem(0, Items.ENCHANTED_GOLDEN_APPLE.getDefaultInstance());
+    }
+}
---
+++ common/src/main/java/org/wishtoday/egar/mixin/goldApple/ItemsMixin.java
@@ -0,0 +1,35 @@
+package org.wishtoday.egar.mixin.goldApple;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.Consumables;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Function;
+
+@Mixin(Items.class)
+public abstract class ItemsMixin {
+
+    @Shadow
+    public static Item registerItem(ResourceKey<Item> key, Function<Item.Properties, Item> factory, Item.Properties properties) {
+        return null;
+    }
+
+    @Shadow
+    private static ResourceKey<Item> vanillaItemId(String name) {
+        return null;
+    }
+
+    @Inject(method = "registerItem(Ljava/lang/String;Lnet/minecraft/world/item/Item$Properties;)Lnet/minecraft/world/item/Item;", at = @At("HEAD"), cancellable = true)
+    private static void onRegisterItem(String name, Item.Properties properties, CallbackInfoReturnable<Item> cir) {
+        if (!"golden_apple".equals(name)) return;
+        cir.setReturnValue(registerItem(vanillaItemId(name), Item::new, properties.enchantable(1)));
+    }
+}
