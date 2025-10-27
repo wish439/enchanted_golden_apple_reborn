a4e22f91d07e9cbae7ba541b74979d1debd73b16
wishtoday238 <wishtoday238@gmail.com>
2025-10-27 17:07:43 +0800

    fix forge1.20.1

--- common/src/main/java/org/wishtoday/egar/mixin/goldApple/EnchantmentHelperMixin.java
+++ common/src/main/java/org/wishtoday/egar/mixin/goldApple/EnchantmentHelperMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
@@ -15,12 +16,14 @@ public class EnchantmentHelperMixin {
             , at = @At(value = "INVOKE"
             , target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private static boolean getPossibleEntries(boolean original, @Local(argsOnly = true) ItemStack stack) {
-        System.out.println("EnchantmentHelper.getPossibleEntries() called");
         return stack.is(Items.BOOK) || stack.is(Items.GOLDEN_APPLE);
     }
     @ModifyExpressionValue(method = "getEnchantmentCost", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;getEnchantmentValue()I"))
     private static int getEnchantmentValue(int original, @Local Item item) {
-        System.out.println("EnchantmentHelper.getEnchantmentValue() called");
         return item.getEnchantmentValue();
+    }
+    @Redirect(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+    private static boolean onGet(ItemStack instance, Item item) {
+        return instance.is(Items.GOLDEN_APPLE) || instance.is(Items.BOOK);
     }
 }
--- forge/src/main/java/org/wishtoday/egar/mixin/ForgeEnchantmentHelperMixin.java
+++ forge/src/main/java/org/wishtoday/egar/mixin/ForgeEnchantmentHelperMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EnchantmentHelper.class)
-public abstract class EnchantmentHelperMixin {
+public abstract class ForgeEnchantmentHelperMixin {
 
     /*@WrapOperation(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private static boolean getAvailableEnchantmentResults(ItemStack instance
@@ -19,10 +19,10 @@ public abstract class EnchantmentHelperMixin {
         else return original.call(instance, item);
     }*/
 
-    @Redirect(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+    /*@Redirect(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private static boolean onGet(ItemStack instance, Item item) {
     
-        /*List<EnchantmentInstance> list = Lists.newArrayList();
+        *//*List<EnchantmentInstance> list = Lists.newArrayList();
         boolean flag = stack.is(Items.BOOK) || stack.is(Items.GOLDEN_APPLE);
         for (Enchantment enchantment : BuiltInRegistries.ENCHANTMENT) {
             if (((!enchantment.isTreasureOnly() || allowTreasure) && enchantment.isDiscoverable() && (enchantment.canApplyAtEnchantingTable(stack) || flag && enchantment.isAllowedOnBooks())) || stack.is(Items.GOLDEN_APPLE)) {
@@ -34,7 +34,7 @@ public abstract class EnchantmentHelperMixin {
                 }
             }
         }
-        cir.setReturnValue(list);*/
+        cir.setReturnValue(list);*//*
         return instance.is(Items.GOLDEN_APPLE) || instance.is(Items.BOOK);
-    }
+    }*/
 }
