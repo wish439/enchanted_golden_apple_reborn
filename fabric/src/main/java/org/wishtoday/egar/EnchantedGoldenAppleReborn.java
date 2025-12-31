1837211ce264d3a71c50fe4e3335d5599ea83b32
wishtoday238 <wishtoday238@gmail.com>
2025-12-31 17:11:35 +0800

    update 1.21.10

--- fabric/src/main/java/org/wishtoday/egar/EnchantedGoldenAppleReborn.java
+++ fabric/src/main/java/org/wishtoday/egar/EnchantedGoldenAppleReborn.java
@@ -10,7 +10,6 @@ public class EnchantedGoldenAppleReborn implements ModInitializer {
         // This method is invoked by the Fabric mod loader when it is ready
         // to load your mod. You can access Fabric and Common code in this
         // project.
-
         // Use Fabric to bootstrap the Common mod.
         Constants.LOG.info("Hello Fabric world!");
         CommonClass.init();
